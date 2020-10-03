8d9dd3e959c572730ac709d91c42a733a049aaf1
seis-635-fall2020 <seis.635.fall2020@gmail.com>
2020-10-02 20:25:14 -0500

    first passing test of PredictableDie

---
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -0,0 +1,32 @@
+package skunk.domain;
+
+public class PredictableDie
+{
+	private int[] theRolls;
+	private int nextInt;
+	private int lastRoll;
+	
+	public PredictableDie()
+	{
+		
+	}
+
+	public PredictableDie(int[] is)
+	{
+		this.theRolls = is;
+		this.nextInt = 0;
+	}
+
+	public void roll()
+	{
+		this.lastRoll = this.theRolls[this.nextInt];
+		
+	}
+
+	public int getLastRoll()
+	{	
+		return this.lastRoll;
+	}
+
+
+}
